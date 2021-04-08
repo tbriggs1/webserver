c79b33f76d45b51f19d94d6d5227b9468389c6a4
Thomas Briggs <57761163+tbriggs1@users.noreply.github.com>
2021-04-08 21:09:10 +0100

    Add files via upload

---
+++ build/precache-manifest.a3bbd8c55258840285baf5f364f112b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a34df5b0457e06f205fc1f28637bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6522dd0ea7347dadecce",
+    "url": "/static/css/main.80168001.chunk.css"
+  },
+  {
+    "revision": "ab27c2708afd29051e24",
+    "url": "/static/js/2.b3c09d3c.chunk.js"
+  },
+  {
+    "revision": "9d93a1feb4281451a203c5d891a62465",
+    "url": "/static/js/2.b3c09d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6522dd0ea7347dadecce",
+    "url": "/static/js/main.61edc3b3.chunk.js"
+  },
+  {
+    "revision": "9bfd6d225e4168839554",
+    "url": "/static/js/runtime-main.22145939.js"
+  }
+]);
