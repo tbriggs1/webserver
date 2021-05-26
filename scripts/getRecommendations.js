fab96ace19f0e7efd6565d23b2163fd321f8bc03
Thomas Briggs <57761163+tbriggs1@users.noreply.github.com>
2021-05-26 16:19:58 +0100

    Add files via upload

--- scripts/getRecommendations.js
+++ scripts/getRecommendations.js
@@ -1,6 +1,6 @@
 $(function () {
     $.ajax({
-        url: 'https://faaed6a347d1.ngrok.io/recommendations/recommended/1/getRecommendedBy/',
+        url: 'https://faaed6a347d1.ngrok.io/recommendations/recommended/1/getRecommendedBy',
         type: 'GET',
         jsonp: 'callback',
         dataType: 'jsonp',
