b1b928ce78dcccb528313ff959b0e2202ed2eeb5
Thomas Briggs <57761163+tbriggs1@users.noreply.github.com>
2021-06-02 08:27:14 +0100

    Add files via upload

--- scripts/getRecommendations.js
+++ scripts/getRecommendations.js
@@ -1,1 +1,18 @@
+let url = 'https://sapsalesdemoblenda859ab94a.hana.ondemand.com/b00588/config.xsodata/LXP_RECOMMENDATIONS?$format=json';
 
+let headers = new Headers();
+
+//headers.append('Content-Type', 'text/json');
+headers.append('Authorization', 'Basic U0ZTQUxFUzAwNTg4MTpTZjQ1NzhTYWxlMDQ5NTE');
+
+fetch(url, {method:'GET',
+        headers: headers,
+        //credentials: 'user:passwd'
+       })
+.then(response => response.json())
+.then(json => console.log(json));
+//.done();
+
+function parseJSON(response) {
+return response.json()
+}
